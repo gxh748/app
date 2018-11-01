8535da54677e4a8dee8358b60d1105862b136404
GXH <1176253771@qq.com>
2018-11-01 17:58:40 +0800

    测试核心知识部分代码

--- heroes/heroes.component.ts
+++ heroes/heroes.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit ,OnChanges,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy} from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
@@ -14,31 +15,6 @@ export class HeroesComponent implements OnInit {
 
   constructor() { }
 
-  ngOnChanges(){
-  	console.log("1ngOnChanges :");
-  }
-
-  ngOnInit() {
-  	console.log("2ngOnInit :called only once");
-  }
-
-  ngDoCheck(){
-  	console.log("3ngDoCheck :");
-  }
-  ngAfterContentInit(){
-  	console.log("4ngAfterContentInit called only once");
-  }
-  ngAfterContentChecked(){
-  	console.log("5ngAfterContentChecked");
-  }
-  ngAfterViewInit(){
-  	console.log("6ngAfterViewInit called only once");
-  }
-  ngAfterViewChecked(){
-  	console.log("7ngAfterViewChecked");
-  }
-  ngOnDestroy(){
-  	console.log("8ngOnDestroy  called only once");
-  }
+  ngOnInit(){}
 
 }
